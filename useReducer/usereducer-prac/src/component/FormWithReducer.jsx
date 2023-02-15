302ec9f373162b0054a7c569b42588b1de00b2d8
motochowdhury <motochowdhury@gmail.com>
2023-02-15 22:35:58 +0600

    form with reducer is added

---
+++ useReducer/usereducer-prac/src/component/FormWithReducer.jsx
@@ -0,0 +1,27 @@
+import React, { useReducer } from "react";
+
+const FormWithReducer = () => {
+  const initialState = {};
+  const reducer = (state, action) => {
+    switch(action.type === "INPUT"){
+        case:
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const submit = (e) => {
+    e.target.preventDefault();
+  };
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="firstName">FirstName</label>
+        <input type="text" name="firstName" />
+        <button type="submit" onSubmit={submit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormWithReducer;
